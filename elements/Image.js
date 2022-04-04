10cdddcae2ccab2e5502b86a7a45c5f30703f3ef
gagyeong <96006761+GYMMX@users.noreply.github.com>
2022-04-05 03:01:49 +0900

    3주차까지 구현된 요소들

---
+++ elements/Image.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import React from "react";
+
+const Image = (props) => {
+  const { shape, src, size } = props;
+  const styles = {
+    src: src,
+    size: size,
+  };
+
+  if (shape === "circle") {
+    return <ImageCircle {...styles}></ImageCircle>;
+  }
+  if (shape === "rectangle") {
+    return (
+      <AspectOutter>
+        <AspectInner {...styles}></AspectInner>
+      </AspectOutter>
+    );
+  }
+  return (
+    <React.Fragment>
+      <ImageDefault {...styles}></ImageDefault>
+    </React.Fragment>
+  );
+};
+
+Image.defaultProps = {
+  shape: "circle",
+  src: "https://media.vlpt.us/images/gagyeong/post/140a1093-5b07-4ec5-8008-cf75b5cc5382/image.png",
+  size: 40,
+};
+
+const ImageDefault = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+const AspectOutter = styled.div`
+  width: 100%;
+  min-width: 250px;
+`;
+
+const AspectInner = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+const ImageCircle = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: var(--size);
+
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+  margin: 4px;
+`;
+
+export default Image;
